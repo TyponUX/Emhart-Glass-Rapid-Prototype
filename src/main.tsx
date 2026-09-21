b17b42ec755e6a23ff793bf7dbd3e9dd90e5776a
TyponUX <nadja.tschopp@prodyna.com>
2026-09-21 12:15:01 +0200

    Update prototype styling and typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/noto-sans";
 import App from "./App";
 import "./styles/globals.css";
 
